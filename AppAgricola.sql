0243e0011b4a47b6c2ba85d8465ea65b22bc5047
MMoll13 <matiasermoll@gmail.com>
2024-11-14 13:08:31 -0300

    AppAgricola.sql

--- AppAgricola.sql
+++ AppAgricola.sql
@@ -21,7 +21,7 @@ INSERT INTO Usuario (id, apellido, nombre, documento, telefono, email) VALUES (7
 INSERT INTO Usuario (id, apellido, nombre, documento, telefono, email) VALUES (8, 'ALVAREZ', 'JULIAN', 40178846, '154992233', 'JALVAREZ9@gmail.com');
 
 SELECT id, apellido, nombre, documento, telefono, email FROM Usuario WHERE id = 1;
-UPDATE Usuario SET apellido = 'MOLL' WHERE id = 1;
+UPDATE Usuario SET apellido = 'PANCHO' WHERE id = 1;
 SELECT id, apellido, nombre, documento, telefono, email FROM Usuario;
 DELETE FROM Usuario WHERE id = 1;
 
@@ -32,16 +32,16 @@ nombre VARCHAR(35) NOT NULL,
 telefono VARCHAR(35),
 email VARCHAR(35),
 ubicacion VARCHAR (35),
-Usuario_id INT
+Usuario_id INT,
 FOREIGN KEY (Usuario_id) REFERENCES Usuario(id)
 );
 
-INSERT INTO Productor (id, apellido, nombre, telefono, email, Usuario_id) VALUES (1, 'MARADONA', 'DIEGO', '154002211', 'lamanoded10s@gmail.com', 'Buenos Aires', 2);
-INSERT INTO Productor (id, apellido, nombre, telefono, email, Usuario_id) VALUES (2, 'MESSI', 'LIONEL', '154112233', 'el10deARG@gmail.com', 'Barcelona', 3);
+INSERT INTO Productor (id, apellido, nombre, telefono, email, ubicacion, Usuario_id) VALUES (1, 'MARADONA', 'DIEGO', '154002211', 'lamanoded10s@gmail.com', 'Buenos Aires', 2);
+INSERT INTO Productor (id, apellido, nombre, telefono, email, ubicacion, Usuario_id) VALUES (2, 'MESSI', 'LIONEL', '154112233', 'el10deARG@gmail.com', 'Barcelona', 3);
 
-SELECT id, apellido, nombre, telefono, email, Usuario_id FROM Productor WHERE id = 1;
+SELECT id, apellido, nombre, telefono, email, ubicacion, Usuario_id FROM Productor WHERE id = 1;
 UPDATE Productor SET apellido = 'MARADONA' WHERE id = 1;
-SELECT id, apellido, nombre, documento, telefono, email FROM Productor;
+SELECT id, apellido, nombre, telefono, email, ubicacion, Usuario_id FROM Productor;
 DELETE FROM Productor WHERE id = 1;
 
 CREATE TABLE Cliente(
@@ -50,16 +50,16 @@ apellido VARCHAR(35) NOT NULL,
 nombre VARCHAR(35) NOT NULL,
 telefono VARCHAR(35),
 email VARCHAR(35),
-Usuario_id INT
+Usuario_id INT,
 FOREIGN KEY (Usuario_id) REFERENCES Usuario(id)
 );
 
-INSERT INTO Cliente (id, apellido, nombre, telefono, email, Usuario_id) VALUES (1, 'DI MARIA', 'ANGEL', '154223311', 'fideoArg@gmail.com', 3);
-INSERT INTO Cliente (id, apellido, nombre, telefono, email, Usuario_id) VALUES (2, 'OTAMENDI', 'NICOLAS', '154121221', 'otanico12@gmail.com', 4);
+INSERT INTO Cliente (id, apellido, nombre, telefono, email, Usuario_id) VALUES (1, 'DI MARIA', 'ANGEL', '154223311', 'fideoArg@gmail.com', 4);
+INSERT INTO Cliente (id, apellido, nombre, telefono, email, Usuario_id) VALUES (2, 'OTAMENDI', 'NICOLAS', '154121221', 'otanico12@gmail.com', 5);
 
-SELECT id, apellido, nombre, telefono, email, Usuario_id INT FROM Cliente WHERE id = 1;
+SELECT id, apellido, nombre, telefono, email, Usuario_id FROM Cliente WHERE id = 1;
 UPDATE Cliente SET apellido = 'DI MARIA' WHERE id = 1;
-SELECT id, apellido, nombre, documento, telefono, email FROM Cliente;
+SELECT id, apellido, nombre, telefono, email, Usuario_id FROM Cliente;
 DELETE FROM Productor WHERE id = 1;
 
 CREATE TABLE Trabajador(
@@ -70,15 +70,14 @@ fecha_contratacion DATE,
 telefono VARCHAR(35),
 email VARCHAR(35),
 Parcela_id INT,
-FOREING KEY (Parcela_id) REFERENCES Parcela (id)
+FOREIGN KEY (Parcela_id) REFERENCES Parcela (id)
 );
 
-INSERT INTO Trabajador VALUES (1, 'MARTINEZ', 'EMILIANO', 2024-10-1, '154312111', 'MQTC26@gmail.com', 1);
-INSERT INTO Trabajador VALUES (2, 'DE PAUL', 'RODRIGO', 2024-10-1,'154663322', 'DPRod94@gmail.com', 2);
-INSERT INTO Trabajador VALUES (3, 'ALVAREZ', 'JULIAN', 2024-10-1,'154992233', 'JALVAREZ9@gmail.com', 2);
-
+INSERT INTO Trabajador VALUES (1, 'MARTINEZ', 'EMILIANO', '2024-10-01', '154312111', 'MQTC26@gmail.com', 1);
+INSERT INTO Trabajador VALUES (2, 'DE PAUL', 'RODRIGO', '2024-10-01','154663322', 'DPRod94@gmail.com', 2);
+INSERT INTO Trabajador VALUES (3, 'ALVAREZ', 'JULIAN', '2024-10-01','154992233', 'JALVAREZ9@gmail.com', 2);
 SELECT id, apellido, nombre, telefono, email, Parcela_id FROM Trabajador WHERE id = 1;
-UPDATE Trabador SET apellido = 'MARTINEZ' WHERE id = 1;
+UPDATE Trabajador SET apellido = 'MARTINEZ' WHERE id = 1;
 SELECT id, apellido, nombre, telefono, email, Parcela_id FROM Trabajador;
 DELETE FROM Trabajador WHERE id = 1;
 
@@ -89,7 +88,7 @@ Productor_id INT,
 Cultivo_id INT,
 Trabajador_id INT,
 FOREIGN KEY (Productor_id) REFERENCES Productor (id),
-FOREIGN KEY (Cultivo_id) REFERENCES Cultivo (id),
+FOREIGN KEY (Cultivo_id) REFERENCES Cultivos (id),
 FOREIGN KEY (Trabajador_id) REFERENCES Trabajador (id)
 );
 
@@ -98,9 +97,9 @@ INSERT INTO Parcela (id, ubicacion, Productor_id, Cultivo_id, Trabajador_id) VAL
 INSERT INTO Parcela (id, ubicacion, Productor_id, Cultivo_id, Trabajador_id) VALUES (3, 'FINCA DOÑA MARIA', 2, 2, 3);
 
 SELECT id, ubicacion, Productor_id, Cultivo_id, Trabajador_id FROM Parcela WHERE id = 1;
-UPDATE Parcela SET ubicación = 'FINCA LOS COMPARTOS' WHERE id = 1;
+UPDATE Parcela SET ubicacion = 'FINCA LOS COMPARTOS' WHERE id = 1;
 SELECT id, ubicacion, Productor_id, Cultivo_id, Trabajador_id FROM Parcela;
-DETELE FROM Parcela WHERE id = 1;
+DELETE FROM Parcela WHERE id = 1;
 
 CREATE TABLE Cultivos (
 id INT PRIMARY KEY NOT NULL,
@@ -113,32 +112,31 @@ FOREIGN KEY (Productor_id) REFERENCES Productor (id),
 FOREIGN KEY (Parcela_id) REFERENCES Parcela (id)
 );
 
-INSERT INTO Cultivos (id, tipo, fecha_plantacion, fecha_cosecha, Productor_ id, Parcela_id) VALUES (1, 'TOMATE', 2024-01-07, 2024-10-07, 1, 1);
-INSERT INTO Cultivos (id, tipo, fecha_plantacion, fecha_cosecha, Productor_ id, Parcela_id) VALUES (2, 'CIRUELA', 2024-01-06, 2024-10-06, 2, 2);
-INSERT INTO Cultivos (id, tipo, fecha_plantacion, fecha_cosecha, Productor_ id, Parcela_id) VALUES (3, 'UVA', 2024-01-05, 2024-10-05, 2, 3);
+INSERT INTO Cultivos (id, tipo, fecha_plantacion, fecha_cosecha, Productor_id, Parcela_id) VALUES (1, 'TOMATE', '2024-01-07', '2024-10-07', 1, 1);
+INSERT INTO Cultivos (id, tipo, fecha_plantacion, fecha_cosecha, Productor_id, Parcela_id) VALUES (2, 'CIRUELA', '2024-01-06', '2024-10-06', 2, 2);
+INSERT INTO Cultivos (id, tipo, fecha_plantacion, fecha_cosecha, Productor_id, Parcela_id) VALUES (3, 'UVA', '2024-01-05', '2024-10-05', 2, 3);
 
-SELECT id, tipo, fecha_plantacion, fecha_cosecha, Productor_ id, Parcela_id FROM Cultivos WHERE id = 1;
-UPDATE Cultivos SET tipo WHERE tipo = 'TOMATE' id = 1;
-SELECT id, tipo, fecha_plantacion, fecha_cosecha, Productor_ id, Parcela_id FROM Cutivos;
-DELETE Cutivos FROM WHERE id = 1;
+SELECT id, tipo, fecha_plantacion, fecha_cosecha, Productor_id, Parcela_id FROM Cultivos WHERE id = 1;
+UPDATE Cultivos SET tipo = 'TOMATE' WHERE id = 1;
+SELECT id, tipo, fecha_plantacion, fecha_cosecha, Productor_id, Parcela_id FROM Cultivos;
+DELETE FROM Cultivos WHERE id = 1;
 
 CREATE TABLE Item (
 id INT PRIMARY KEY NOT NULL,
 categoria VARCHAR(35),
 descripcion VARCHAR(500),
-precio DECIMAL (10, 2),
+precio FLOAT (10, 2),
 fecha_elaboracion DATE,
 Productor_id INT,
 FOREIGN KEY (Productor_id) REFERENCES Productor (id)
 );
 
-INSERT INTO Item (id, categoria, descripcion, precio, fecha_elaboración, Productor_id) VALUES (1, 'VINO', 'Vino de cosecha de selección, sello: finca Doña maria', '12111,00', 2024-10-1, 1 );
-INSERT INTO Item (id, categoria, descripcion, precio, fecha_elaboración, Productor_id) VALUES (2, 'MIEL', 'Miel de abeja natural, traida a ustedes desde finca Los Compartos', '7000,00', 2024-10-1, 2);
-INSERT INTO Item (id, categoria, descripcion, precio, fecha_elaboración, Productor_id) VALUES (3, 'TOMATES ENVASADOS','Productos Artesanales', '¡Los mejores tomates que probaras!', '8500,00', 2024-10-1, 2 );
-
-SELECT id, categoria, descripcion, precio, fecha_elaboración, Productor_id FROM Item WHERE id = 1;
-UPDATE Item SET Categoria = 'VINO'WHERE id = 1;
-SELECT id, categoria, descripcion, precio, fecha_elaboración, Productor_id FROM Item;
+INSERT INTO Item (id, categoria, descripcion, precio, fecha_elaboracion, Productor_id) VALUES (1, 'VINO', 'Vino de cosecha de selección, sello: finca Doña maria', 12111,00, '2024-10-01', 1 );
+INSERT INTO Item (id, categoria, descripcion, precio, fecha_elaboracion, Productor_id) VALUES (2, 'MIEL', 'Miel de abeja natural, traida a ustedes desde finca Los Compartos', 07000,00, '2024-10-01', 2);
+INSERT INTO Item (id, categoria, descripcion, precio, fecha_elaboracion, Productor_id) VALUES (3, 'TOMATES ENVASADOS','Productos Artesanales', '¡Los mejores tomates que probaras!', 08500,00, '2024-10-01', 2 );
+SELECT id, categoria, descripcion, precio, fecha_elaboracion, Productor_id FROM Item WHERE id = 1;
+UPDATE Item SET Categoria = 'VINO' WHERE id = 1;
+SELECT id, categoria, descripcion, precio, fecha_elaboracion, Productor_id FROM Item;
 DELETE FROM Item WHERE id = 1;
 
 CREATE TABLE Pedido (
@@ -154,11 +152,11 @@ FOREIGN KEY (Productor_id) REFERENCES Productor (id),
 FOREIGN KEY (Item_id) REFERENCES Item (id)
 );
 
-INSERT INTO Pedido (id, fecha_pedido, cantidad, pago, Cliente_id, Productor_id, Item_id) VALUES (1, 2024-10-06, 3, '36333,00', 1, 1, 1);
-INSERT INTO Pedido (id, fecha_pedido, cantidad, pago, Cliente_id, Productor_id, Item_id) VALUES (2, 2024-10-06, 5, '42500', 2, 2, 3);
+INSERT INTO Pedido (id, fecha_pedido, cantidad, pago, Cliente_id, Productor_id, Item_id) VALUES (1, '2024-10-06', 3, 36333,00, 1, 1, 1);
+INSERT INTO Pedido (id, fecha_pedido, cantidad, pago, Cliente_id, Productor_id, Item_id) VALUES (2, '2024-10-06', 5, 42500,00, 2, 2, 3);
 
 SELECT id, fecha_pedido, cantidad, pago, Cliente_id, Productor_id, Item_id FROM Pedido WHERE id = 1;
-UPDATE Pedido SET fecha_pedido =2024-10-06 WHERE id = 1;
+UPDATE Pedido SET fecha_pedido = '2024-10-06' WHERE id = 1;
 SELECT id, fecha_pedido, cantidad, pago, Cliente_id, Productor_id, Item_id FROM Pedido;
 DELETE FROM Pedido WHERE id = 1;
 
